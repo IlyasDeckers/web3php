16322a1b0e319c9636f8e9ae9e2e9e7095f18c0f
IlyasDeckers <ilyas@phasehosting.io>
2018-02-21 22:03:11 +0100

    Initial Commit

---
+++ src/Web3PHPServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+namespace IlyasDeckers\Web3PHP;
+
+use Illuminate\Support\ServiceProvider;
+
+class Web3PHPServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
