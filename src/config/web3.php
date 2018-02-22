f0290d760ec495e4528b39d676f8fa16d7377582
IlyasDeckers <ilyas@phasehosting.io>
2018-02-22 11:16:50 +0100

    Publish Config

--- src/config/web3.php
+++ src/config/web3.php
@@ -5,7 +5,7 @@ return [
      * Please provide a valid url to connect to a Ethereum node.
      * If you can't run a private Geth/Parity node you could use https://infura.io.
      */
-    'url' => env('WEB3_URL'),
-    'port' => env('WEB3_PORT')
+    'url' => env('WEB3_URL', '127.0.0.1'),
+    'port' => env('WEB3_PORT', 8545)
 
-];
+]; 
